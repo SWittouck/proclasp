6080c9e17923b8bdd6f075decc2ddb40eb828675
SWittouck <stijn.wittouck@uantwerpen.be>
2023-07-07 13:27:12 +0200

    Allow download of other data types

--- src/scripts/download_fnas.sh
+++ src/scripts/download_fnas.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
 
+# set commandline arguments
 fin=$1
 dout=$2
+filetype=${3:-_genomic.fna} # default is "_genomic.fna.gz"
 
+# create output folder
 [ -d $dout ] || mkdir -p $dout
 
+# loop over assembly accessions
 for acc in $(cat $fin | cut -f 1) ; do
+
   echo $acc
-  url=ftp.ncbi.nlm.nih.gov/genomes/all/${acc:0:3}/${acc:4:3}/${acc:7:3}/${acc:10:3}/${acc}*/${acc}*_genomic.fna.gz
+  url=ftp.ncbi.nlm.nih.gov/genomes/all/${acc:0:3}/${acc:4:3}/${acc:7:3}/${acc:10:3}/${acc}*/${acc}*${filetype}.gz
+
   exitcode=1 
   attempt=1
   while [[ $exitcode -ne 0 ]] && [[ $attempt -le 5 ]] ; do
@@ -15,7 +21,7 @@ for acc in $(cat $fin | cut -f 1) ; do
     rsync --copy-links --times --verbose \
       --exclude *_cds_from_genomic.fna.gz \
       --exclude *_rna_from_genomic.fna.gz \
-      rsync://$url $dout > /dev/null 2>&1
+      rsync://$url $dout # > /dev/null 2>&1
     exitcode=$?
     attempt=$((attempt + 1))
     # echo $exitcode $attempt
@@ -24,4 +30,5 @@ for acc in $(cat $fin | cut -f 1) ; do
       echo $acc >> $dout/failed.txt
     fi
   done
+
 done
